a9bc4d6e9708ca65cdc012ce4cccc903671b7777
Daniel Valesi <danielvalesi@gmail.com>
2016-10-06 16:20:57 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var app = express();
+app.use(bodyparser.json());
+
+
+
+// Metodo GET
+// Vai trazer os parametros e colocar dentro da variavel request... tudo do cliente para oervidor
+// response é resposta para o cliente
+// quando acessar a pagina inicial da um hello word
+/*
+app.get('/', function(request, response) {
+	response.send('Hello World');
+});
+*/
+
+var tarefas = [];
+
+
+// GET (READ)
+app.get('/api/todo', function(request, response) {
+	response.json(tarefas);
+});
+
+
+
+// POST (CREATE)
+app.post('/api/todo', function(request, response) {
+	var tarefa = {
+		"id" : new Date().getTime(),
+		"texto" : request.body.texto,
+		"concluida" : false
+
+	};
+
+	tarefas.push(tarefa);
+
+	response.send(200); // OK HTTP
+});
+
+
+
+
+// PUT (UPDATE)
+app.put('/api/todo', function(request, response) {
+	response.json(cliente);
+});
+
+
+
+// DELETE (DELETE)
+app.delete('/api/todo', function(request, response) {
+	response.json(cliente);
+});
+
+
+/*
+
+ app.get('/cliente', function(request, response) {
+
+ 	var cliente = {
+ 		id: 1,
+ 		nome: "fulano"
+ 	};
+ 	response.json(cliente);
+ });
+
+*/
+
+
+app.listen('3000', function(){
+	console.log('servidor iniciado na porta 3000');
+});
